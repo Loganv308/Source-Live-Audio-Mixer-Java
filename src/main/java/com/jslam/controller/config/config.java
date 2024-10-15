2dc37f4454ed4932b730c29ac63107f54c772fbf
Loganv308 <loganv308@gmail.com>
2024-10-14 19:33:29 -0500

    Added persistence for settings menu checkboxes, added logging to the new steps.

--- src/main/java/com/jslam/controller/config/config.java
+++ src/main/java/com/jslam/controller/config/config.java
@@ -1,5 +1,11 @@
 package com.jslam.controller.config;
 
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -10,13 +16,22 @@ public class Config {
     private Boolean startEnabled = false;
     private Boolean minSystemTray = false;
     private Boolean startMini = false;
+    private String path = "src\\main\\java\\com\\jslam\\controller\\config\\config.xml";
     
+    
+    // public Config(){};
+
     // Default Constructor
-    public Config(Boolean logErrors, Boolean startEnabled, Boolean minSystemTray, Boolean startMini) {
-        this.logErrors = logErrors;
-        this.startEnabled = startEnabled;
-        this.minSystemTray = minSystemTray;
-        this.startMini = startMini;
+    public Config() {
+
+    }
+
+    public String getPath(){
+        return this.path;
+    }
+
+    public void setPath(String path){
+        this.path = path;
     }
 
     // Getter - Setter for log errors with XmlElement annotation
@@ -63,5 +78,42 @@ public class Config {
     @Override
     public String toString() {
         return "Config [logErrors=" + logErrors + ", startEnabled=" + startEnabled + ", minSystemTray=" + minSystemTray + ", startMini=" + startMini + "]" ;
+    }
+
+    public void saveXMLToFile(){
+        try{
+            JAXBContext context = JAXBContext.newInstance(Config.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            // Save the config to an XML file
+            marshaller.marshal(this, new File(this.path));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadXMLFromFile() {
+        try{ 
+            JAXBContext context = JAXBContext.newInstance(Config.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            
+            // Reads XML config file
+            File file = new File("src\\main\\java\\com\\jslam\\controller\\config\\config.xml");
+
+            if(file.exists()) {
+                this.copy((Config) unmarshaller.unmarshal(file));
+            } 
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void copy(Config config) {
+        this.logErrors = config.logErrors;
+        this.minSystemTray = config.minSystemTray;
+        this.startEnabled = config.startEnabled;
+        this.startMini = config.startMini;
     }
 }
